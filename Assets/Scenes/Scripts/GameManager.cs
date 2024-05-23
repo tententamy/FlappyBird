8531a3d26609b3c0d3a07a7d9e65a996a6a27408
Nguyễn Minh Tâm <137066831+tententamy@users.noreply.github.com>
2024-05-23 20:26:48 +0700

    demo finish
    demo finish

---
+++ Assets/Scenes/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+
+    private int score;
+
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipe = FindObjectsOfType<Pipes>(); 
+        for(int i = 0; i < pipe.Length; i++)
+        {
+            Destroy(pipe[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
+    }
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
